dd4b8128a8f94883fb98d9c195a439ce02604d00
g5r1b <katrinatan91@yahoo.com>
2020-03-20 19:57:10 -0700

    Made minor styling changes to html, Added images for the nodes, started processing data for nodelink graph

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ let area = new Area({parentElement: '#revenue-overall'});
 let nodeLink = new NodeLink({parentElement: '#movie-actors'});
 let histogram = new Histogram({parentElement: '#movie-era'});
 
+let nodelinkDataProcessor = new NodeLinkDataProcessor();
+
+let nodes = [];
+let links = [];
+
 Promise.all([
   d3.csv('data/disney_revenue.csv'),
   d3.csv('data/disney-movies.csv'),
@@ -19,7 +24,12 @@ Promise.all([
     val.parks_resorts = +val.parks_resorts;
     val.media = +val.media;
     val.total = +val.total;
-  })
+  });
+
+  nodelinkDataProcessor.processMovieData(moviesRaw, nodes);
+  nodelinkDataProcessor.processVoiceActorData(actorsRaw, nodes, links);
+  console.log(nodes);
+  console.log(links);
 
   area.initVis({data: revenueRaw});
-})
+});
---
+++ js/nodelinkDataProcessor.js
@@ -0,0 +1,56 @@
+class NodeLinkDataProcessor {
+    constructor () {}
+
+    processMovieData(moviesRaw, nodes) {
+        moviesRaw.forEach(movie => {
+            let releaseDate = new Date(movie["release_date"]);
+            let disneyEra = this.getDisneyEra(releaseDate.getFullYear());
+            let movieObj = {
+                type: "movie",
+                movie_title: movie["movie_title"],
+                release_date: releaseDate,
+                rating: movie["rating"],
+                box_office: movie["box_office"],
+                era: disneyEra
+            };
+            nodes.push(movieObj);
+        });
+    }
+
+    getDisneyEra(year) {
+        if (year >= 1937 && year <= 1942) {
+            return 'Golden Age';
+        } else if (year >= 1943 && year <= 1949) {
+            return 'Wartime Era';
+        } else if (year >= 1950 && year <= 1969) {
+            return 'Silver Age';
+        } else if (year >= 1970 && year <= 1988) {
+            return 'Dark Age';
+        } else if (year >= 1989 && year <= 1999) {
+            return 'Disney Renaissance';
+        } else if (year >= 2000 && year <= 2009) {
+            return 'Post-Renaissance';
+        } else if (year >= 2010 && year <= 2019) {
+            return 'Second Disney Renaissance';
+        }
+    }
+
+    processVoiceActorData(actorsRaw, nodes, links) {
+        actorsRaw.forEach(vActor => {
+
+            let vActorNode = {
+                type: "actor",
+                name: vActor['voice-actor']
+            };
+            nodes.push(vActorNode);
+
+            let link = {
+                source: vActor['voice-actor'],
+                target: vActor['movie'],
+                role: vActor['character']
+            };
+            links.push(link);
+
+        });
+    }
+}
